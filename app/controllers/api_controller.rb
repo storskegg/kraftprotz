5d540344dea83b0e351adab20c95dd2394cf7694
Liam Conrad <liam@w4pho.net>
2024-12-15 14:33:20 -0600

    still a bit repeaty

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,14 +1,26 @@
 class ApiController < ActionController::API
   def small
-    page_url = URI.parse(request.base_url + root_path + 'tests/small')
-
-    render json: get_body_as_words(page_url).to_json
+    begin
+      page_url = URI.parse(request.base_url + root_path + 'tests/small')
+      words = get_body_as_words(page_url)
+      render json: words.to_json
+    rescue => e
+      Rails.logger.error "Error fetching page: #{e.message}"
+      head 500
+      render json: { error: "An error occurred", error_message: "#{e.message}" }.to_json
+    end
   end
 
   def nietzsche
-    page_url = URI.parse(request.base_url + root_path + 'tests/nietzsche')
-
-    render json: get_body_as_words(page_url).to_json
+    begin
+      page_url = URI.parse(request.base_url + root_path + 'tests/nietzsche')
+      words = get_body_as_words(page_url)
+      render json: words.to_json
+    rescue => e
+      Rails.logger.error "Error fetching page: #{e.message}"
+      head 500
+      render json: { error: "An error occurred", error_message: "#{e.message}" }.to_json
+    end
   end
 end
 
@@ -26,7 +38,12 @@ def get_body_as_words(page_url)
       "Accept-Language" => "en-US,en;q=0.5",
       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0"
     },
+    open_timeout: 500.in_milliseconds,
+    write_timeout: 1.seconds,
+    read_timeout: 3.seconds
   })
+
+  response.inspect
 
   doc = Nokogiri::HTML5(response.body.to_s)
 
@@ -37,13 +54,13 @@ def get_body_as_words(page_url)
       Rails.logger.warn "Skipping empty node"
       next
     else
-      words_s = node_text.downcase.gsub(/\W+/, ' ')
+      words_s = node_text.downcase.gsub(/[\W\d_]+/, ' ')
       hash_s = Digest::SHA512.hexdigest words_s
       words = words_s.split
       Rails.logger.warn "Set [#{hash_s}]: #{words}"
       accumulator << words
     end
   end
-  # doc.css('body').inner_text.to_s.downcase.gsub(/\W+/, ' ').split
+
   accumulator
 end
