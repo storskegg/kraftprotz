88bc7f84afdb64852d674c143a0f099ee40a365f
Liam Conrad <liam@w4pho.net>
2024-12-14 19:19:20 -0600

    add nietzsche api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,16 +1,25 @@
 class ApiController < ActionController::API
   def small
     page_url = URI.parse(request.base_url + root_path + 'tests/small')
-    response = HTMLBody.get(page_url)
 
-    doc = Nokogiri::HTML5(response.body.to_s)
-    word_list = doc.css('body').inner_text.to_s.downcase.gsub(/\W+/, ' ').split
+    render json: get_body_as_words(page_url).to_json
+  end
 
-    render json: word_list.to_json
+  def nietzsche
+    page_url = URI.parse(request.base_url + root_path + 'tests/nietzsche')
+
+    render json: get_body_as_words(page_url).to_json
   end
 end
 
 class HTMLBody
   include HTTParty
   format :html
+end
+
+def get_body_as_words(page_url)
+  response = HTMLBody.get(page_url)
+
+  doc = Nokogiri::HTML5(response.body.to_s)
+  doc.css('body').inner_text.to_s.downcase.gsub(/\W+/, ' ').split
 end
