f4f67e7fc8fc623e98ae5e9a9eef1cbe9619b0e6
Liam Conrad <liam@w4pho.net>
2024-12-19 12:50:51 -0600

    scratch: handing pdf's

--- scratch/crawl_wikipedia.rb
+++ scratch/crawl_wikipedia.rb
@@ -1,10 +1,11 @@
 require 'spidr'
 
-url_wikipedia = 'https://en.wikipedia.org/wiki/Main_Page'
+# url_wikipedia = 'https://en.wikipedia.org/wiki/Main_Page'
+url_wikipedia = 'http://localhost:3000'
 
 links_count = 0
-doc_count = 0
 page_count = 0
+pdf_count = 0
 
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0"
 filter_wikipedia = /(:|\.php|^\/w\/)/
@@ -33,26 +34,18 @@ begin
       end
     end
 
-    # spider.every_html_doc do |doc|
-    #   doc_count += 1
-    #
-    #   puts '=' * 10, "[DOC|#{doc_count}]", '=' * 40
-    #   puts "#{doc.to_s}"
-    #
-    #   spider.pause! unless doc_count < 2
-    # end
-
     spider.every_html_page do |page|
       page_count += 1
       puts '=' * 25 + "[PAGE|#{page_count}]" + '=' * 25
 
       if page.headers['content-language'].nil?
+        puts "[SKIP_LANG|#{page_count}] Absent Content-Language header for page: #{page.url}"
         return
       end
 
       lang = page.headers['content-language'].join(' ').downcase
 
-      if lang.eql? 'en'
+      if lang.include? 'en'
         puts "[ACCEPT_LANG|#{page_count}]", "    '#{page.body[0..80].gsub(/\n+/, ' ').strip}...'"
       else
         puts "[SKIP_LANG|#{page_count}] Skipping non-English page: #{page.url}"
@@ -62,7 +55,19 @@ begin
 
       spider.pause! unless page_count < 20
     end
+
+    spider.every_pdf_page do |page|
+      pdf_count += 1
+      puts '=' * 35 + "[PDF|#{pdf_count}]" + '=' * 15
+
+      puts "[ACCEPT_PDF|#{pdf_count}]", "    '#{page.body[0..80].gsub(/\n+/, ' ').strip}...'"
+
+      spider.pause! unless pdf_count < 2
+    end
   end
+rescue Spidr::Error => e
+  puts "An error occurred during crawling: #{e.message}"
+  exit 2
 rescue StandardError => e
   puts "An error occurred: #{e.message}"
   exit 1
